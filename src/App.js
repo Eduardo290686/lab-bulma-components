6be606199bfd7414dd219c6d39360a607f744ffe
Eduardo Álvarez <ceciliorubes@gmail.com>
2020-04-18 11:37:37 +0200

    Sign up terminado.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import "./App.css";
 import Navbar from "./navbar/Navbar";
 import Formfield from "./formfield/Formfield";
 import Button from "./button/Button";
+import 'bulma/css/bulma.css';
 
 class App extends React.Component {
   render() {
     return (
       <React.Fragment>
         <Navbar></Navbar>
-        <Formfield firstLabel="Name" secondLabel="Password"></Formfield>
+        <div className="form">
+          <Formfield label="Name" type="text" placeholder="Your name"></Formfield>
+          <Formfield label="Email" type="email" placeholder="Your email"></Formfield>
+          <Formfield label="Password" type="password" placeholder="Your password"></Formfield>
+          <Button buttonClass="button is-success right-button" buttonText="Submit"></Button>
+        </div>
       </React.Fragment>
     );
   }
--- src/formfield/Formfield.js
+++ src/formfield/Formfield.js
@@ -5,20 +5,10 @@ import 'bulma/css/bulma.css';
 class Formfield extends Component {
   render() {
     return (
-      <div className="form-container">
-        <div className="form">
-          <div className="field">
-            <label className="label">{this.props.firstLabel}</label>
-            <div className="control">
-              <input className="input" type="text" placeholder="e.g Alex Smith" />
-            </div>
-          </div>
-          <div className="field">
-            <label className="label">{this.props.secondLabel}</label>
-            <div className="control">
-              <input className="input" type="email" placeholder="e.g. alexsmith@gmail.com" />
-            </div>
-          </div>
+      <div className="field">
+        <label className="label">{this.props.label}</label>
+        <div className="control">
+          <input className="input" type={this.props.type} placeholder={this.props.placeholder} />
         </div>
       </div>
     );
